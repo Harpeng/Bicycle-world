ec2bfc0976a67d78e41815e80cb5cb8bf928f5d3
Harpeng <gondarev75@gmail.com>
2022-10-22 16:39:04 +0300

    feature/slider

---
+++ src/components/utils/slider.js
@@ -0,0 +1,17 @@
+new Swiper('.cover', {
+    navigation: {
+        nextEl: '.cover__button_strelka_right',
+        prevEl: '.cover__button_strelka_left'
+    },
+
+    slidesPerView: 2,
+
+    slidesPerGroup: 1,
+
+    initialSlide: 0,
+
+    loop: true,
+
+    loopedSlides: 3,
+
+});
