6c6f6f67b1f3208ca9cba69c8947792d9c5eb159
Harpeng <gondarev75@gmail.com>
2022-10-22 23:47:35 +0300

    slider_is_done

---
+++ src/components/slider.js
@@ -0,0 +1,73 @@
+let imgSlider = new Swiper('.cover__container-img', {
+    navigation: {
+        nextEl: '.cover__button_strelka_right',
+        prevEl: '.cover__button_strelka_left'
+    },
+
+    slidesPerView: 2,
+
+    spaceBetween: 30,
+
+    slidesPerGroup: 1,
+
+    initialSlide: 0,
+
+    loop: true,
+
+    loopedSlides: 2,
+
+    allowTouchMove:false,
+
+});
+
+
+let textSlider = new Swiper('.cover__container-text', {
+    navigation: {
+        nextEl: '.cover__button_strelka_right',
+        prevEl: '.cover__button_strelka_left'
+    },
+
+    slidesPerView: 1,
+
+    spaceBetween: 30,
+
+    slidesPerGroup: 1,
+
+    initialSlide: 0,
+
+    loop: true,
+
+    loopedSlides: 2,
+
+    allowTouchMove:false,
+
+});
+
+let iconSlider = new Swiper('.cover__container-icon', {
+    navigation: {
+        nextEl: '.cover__button_strelka_right',
+        prevEl: '.cover__button_strelka_left'
+    },
+
+    slidesPerView: 1,
+
+    slidesPerGroup: 1,
+
+    initialSlide: 0,
+
+    loop: true,
+
+    loopedSlides: 1,
+
+    effect: 'fade',
+
+    fadeEffect: {
+        crossFade: true
+    },
+
+    allowTouchMove:false,
+
+});
+
+textSlider.controller.control = imgSlider;
+imgSlider.controller.control = textSlider;
